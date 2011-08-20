bae4a9f551b8dcfc4fbc8a21236293288525aeb5
Bauffman <davy@spoon-library.com>
2011-08-20 09:15:02 +0200

    The template unit tests now reflect the environment changes.

--- tests/template.php
+++ tests/template.php
@@ -11,7 +11,7 @@
 
 use spoon\template\Autoloader;
 use spoon\template\Template;
-use spoon\template\Extension;
+use spoon\template\Environment;
 
 require_once realpath(dirname(__FILE__) . '/../') . '/autoloader.php';
 require_once 'PHPUnit/Framework/TestCase.php';
@@ -27,18 +27,13 @@ class TemplateTest extends PHPUnit_Framework_TestCase
 	{
 		parent::setUp();
 		Autoloader::register();
-		$this->template = new Template();
+		$this->template = new Template(new Environment());
 	}
 
 	protected function tearDown()
 	{
 		$this->template = null;
 		parent::tearDown();
-	}
-
-	public function testAddExtension()
-	{
-		// @todo not yet implemented
 	}
 
 	public function testAssign()
@@ -59,44 +54,6 @@ class TemplateTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($object->email, $this->template->get('email'));
 		$this->template->assign('person', $object);
 		$this->assertEquals($object, $this->template->get('person'));
-
-		// @todo add more checks?
-	}
-
-	public function testDisableAutoEscape()
-	{
-		$this->template->disableAutoEscape();
-		$this->assertFalse($this->template->isAutoEscape());
-	}
-
-	public function testDisableAutoReload()
-	{
-		$this->template->disableAutoReload();
-		$this->assertFalse($this->template->isAutoReload());
-	}
-
-	public function testDisableDebug()
-	{
-		$this->template->disableDebug();
-		$this->assertFalse($this->template->isDebug());
-	}
-
-	public function testEnableAutoEscape()
-	{
-		$this->template->enableAutoEscape();
-		$this->assertTrue($this->template->isAutoEscape());
-	}
-
-	public function testEnableAutoReload()
-	{
-		$this->template->enableAutoReload();
-		$this->assertTrue($this->template->isAutoReload());
-	}
-
-	public function testEnableDebug()
-	{
-		$this->template->enableDebug();
-		$this->assertTrue($this->template->isDebug());
 	}
 
 	public function  testGet()
@@ -105,79 +62,10 @@ class TemplateTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('Davy Hellemans', $this->template->get('name'));
 	}
 
-	public function testGetCache()
-	{
-		$this->assertEquals('.', $this->template->getCache());
-	}
-
-	public function testGetCacheFilename()
-	{
-		$expected = $this->template->getCacheFilename('party.tpl');
-		$result = $this->template->getCacheFilename('../tests/party.tpl');
-		$this->assertEquals($expected, $result);
-	}
-
-	public function testGetCharset()
-	{
-		$this->assertEquals('utf-8', $this->template->getCharset());
-	}
-
-	public function testGetExtension()
-	{
-		// @todo not yet implemented
-	}
-
-	public function testGetExtensions()
-	{
-		// @todo not yet implemented
-	}
-
-	public function testIsAutoEscape()
-	{
-		$this->assertTrue($this->template->isAutoEscape());
-	}
-
-	public function testIsAutoReload()
-	{
-		$this->assertTrue($this->template->isAutoReload());
-	}
-
-	public function testIsDebug()
-	{
-		$this->assertFalse($this->template->isDebug());
-	}
-
-	public function testIsChanged()
-	{
-		$this->assertFalse($this->template->isChanged(__FILE__, time()));
-	}
-
 	public function testRemove()
 	{
 		$this->template->assign('name', 'Davy Hellemans');
 		$this->template->remove('name');
 		$this->assertEquals(null, $this->template->get('name'));
-	}
-
-	public function testRemoveExtension()
-	{
-		// @todo not yet implemented
-	}
-
-	public function testSetCache()
-	{
-		$this->template->setCache(__DIR__);
-		$this->assertEquals(__DIR__, $this->template->getCache());
-	}
-
-	public function testSetCharset()
-	{
-		$this->template->setCharset('iso-8859-1');
-		$this->assertEquals('iso-8859-1', $this->template->getCharset());
-	}
-
-	public function testSetExtensions()
-	{
-		// @todo not yet implemented
 	}
 }
