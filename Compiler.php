6688e8d754d698f816663ab8dfd6d92762222aba
Bauffman <davy@spoon-library.com>
2011-10-20 16:46:56 +0200

    The compiler now uses the Writer object to write the cached php template.

--- Compiler.php
+++ Compiler.php
@@ -9,7 +9,10 @@
 * file that was distributed with this source code.
 */
 
-namespace spoon\template;
+namespace Spoon\Template;
+use Spoon\Template\Writer;
+use Spoon\Template\Parser\Variable;
+use Spoon\Template\Parser\Text;
 
 /**
  * Class the compiles template files to cached php files.
@@ -35,14 +38,12 @@ class Compiler
 	/**
 	 * Template instance.
 	 *
-	 * @var Template
+	 * @var Spoon\Template
 	 */
 	protected $template;
 
 	/**
-	 * Constructor.
-	 *
-	 * @param Template $template The template object.
+	 * @param Spoon\Template $template The template object.
 	 * @param string $filename The location of the template you wish to compile.
 	 */
 	public function __construct(Template $template, $filename)
@@ -52,12 +53,74 @@ class Compiler
 	}
 
 	/**
+	 * Compiles the template based on the tokens found in it.
+	 *
+	 * @return string
+	 */
+	protected function compile()
+	{
+		$lexer = new Lexer($this->template->getEnvironment());
+		$stream = $lexer->tokenize(file_get_contents($this->filename), basename($this->filename));
+
+		$writer = new Writer();
+		$writer->write("<?php\n");
+		$writer->write("\n");
+		$writer->write('namespace Spoon\Template;' . "\n");
+		$writer->write("\n");
+		$writer->write('/* ' . $this->filename . ' */' . "\n");
+
+		$class = $this->template->getEnvironment()->getCacheFilename($this->filename);
+		$class = substr($class, 0, -8) . '_Template';
+		$writer->write("class $class extends Template\n");
+		$writer->write("{\n");
+		$writer->indent();
+		$writer->write('protected function display(array $context)' . "\n");
+		$writer->write("{\n");
+		$writer->indent();
+
+		$token = $stream->getCurrent();
+		while(!$stream->isEof())
+		{
+			switch($token->getType())
+			{
+				case Token::TEXT:
+					$text = new Text($stream, $this->template->getEnvironment());
+					$writer->write($text->compile(), $token->getLine());
+					break;
+
+				case Token::VAR_START:
+					$variable = new Variable($stream, $this->template->getEnvironment());
+					$writer->write($variable->compile(), $token->getLine());
+					break;
+			}
+
+			if($token->getType() !== Token::EOF)
+			{
+				$token = $stream->next();
+			}
+
+			else
+			{
+				break;
+			}
+		}
+
+		$writer->outdent();
+		$writer->write("}\n");
+		$writer->outdent();
+		$writer->write("}\n");
+
+		$this->source = $writer->getSource();
+		return $this->source;
+	}
+
+	/**
 	 * Writes the parsed template to its cache file.
 	 */
 	public function write()
 	{
 		// load filename into string
-		$this->source = file_get_contents($this->filename);
+		$this->source = $this->compile();
 
 		// file location
 		$file = $this->template->getEnvironment()->getCache() . '/';
