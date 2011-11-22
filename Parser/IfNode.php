48d755e46c97fc90c5020b6ef0e2ddea6453897a
Bauffman <davy@spoon-library.com>
2011-11-22 09:12:32 +0100

    Second version of the if node. Now allowing more complex ifs.

--- Parser/IfNode.php
+++ Parser/IfNode.php
@@ -24,9 +24,28 @@ use Spoon\Template\Writer;
 class IfNode
 {
 	/**
+	 * Counter for all opened brackets.
+	 *
+	 * @var int
+	 */
+	protected $brackets;
+
+	/**
 	 * @var Spoon\Template\Environment
 	 */
 	protected $environment;
+
+	/**
+	 * Starting line of the if node.
+	 *
+	 * @var int
+	 */
+	protected $line;
+
+	/**
+	 * @var string
+	 */
+	protected $output;
 
 	/**
 	 * @var Spoon\Template\TokenStream
@@ -50,24 +69,116 @@ class IfNode
 	 */
 	public function compile(Writer $writer)
 	{
-		$token = $this->stream->next();
-		$line = $token->getLine();
+		$this->line = $this->stream->next()->getLine();
 
-		$this->stream->expect(Token::NAME);
+		$this->process();
+		$this->output = trim($this->output);
+		$this->output = str_replace(array('( ', '! '), array('(', '!'), $this->output);
 
-		// not a valid name (needs to start with "$")
-		if(substr($token->getValue(), 0, 1) != '$')
+		if($this->brackets != 0)
 		{
-			throw new SyntaxError(
-				'Expecting variable', $line, $this->stream->getFilename()
-			);
+			// @todo create syntax error
+			exit('Not all opened brackets were properly closed');
 		}
 
-		$output = 'if(';
-		$subVariable = new SubVariable($this->stream, $this->environment);
-		$output .= $subVariable->compile() . '):' . "\n";
-
-		$writer->write($output, $line);
+		$writer->write('if(' . $this->output . '):');
 		$writer->indent();
+	}
+
+	/**
+	 * Processes the if syntax.
+	 *
+	 * @todo document me inline
+	 */
+	protected function process()
+	{
+		$token = $this->stream->getCurrent();
+
+		if($token->test(Token::NAME))
+		{
+			if(substr($token->getValue(), 0, 1) != '$')
+			{
+				// @todo create syntax error
+				exit('Variables need to start with $');
+			}
+
+			else
+			{
+				$subVariable = new SubVariable($this->stream, $this->environment);
+				$this->output .= ' ' . $subVariable->compile();
+				$this->stream->previous();
+			}
+		}
+
+		elseif($token->test(Token::NUMBER))
+		{
+			$this->output .= ' ' . $token->getValue();
+		}
+
+		elseif($token->test(Token::OPERATOR))
+		{
+			/*
+			 * Expression regex pattern for:
+			 * or, and, ==, !=, <, >, >=, <=, +, -, *, /, %
+			 */
+			switch($token->getValue())
+			{
+				case 'or':
+					$this->output .= ' ||';
+					break;
+
+				case 'and':
+					$this->output .= ' &&';
+					break;
+
+				case 'not':
+					$this->output .= ' !';
+					break;
+
+				case '==':
+				case '!=':
+				case '>':
+				case '>=':
+				case '<':
+				case '<=':
+				case '*':
+				case '+':
+				case '-':
+				case '/':
+				case '%':
+				case '~':
+					$this->output .= ' ' . $token->getValue();
+					break;
+
+				default:
+					// @todo create syntax error
+					var_dump('unknown operator');
+					var_dump($token->getValue());
+					exit;
+			}
+		}
+
+		elseif($token->test(Token::PUNCTUATION, '('))
+		{
+			$this->output .= ' (';
+			$this->brackets += 1;
+		}
+
+		elseif($token->test(Token::PUNCTUATION, ')'))
+		{
+			$this->output .= ')';
+			$this->brackets -= 1;
+		}
+
+		elseif($token->test(Token::STRING))
+		{
+			$this->output .= " '" . str_replace("'", "\'", ($token->getValue())) . "'";
+		}
+
+		$token = $this->stream->next();
+		if(!$token->test(Token::BLOCK_END))
+		{
+			$this->process();
+		}
 	}
 }
